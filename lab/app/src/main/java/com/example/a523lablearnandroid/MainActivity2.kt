852223e6c74a035869f91e962f3aba06a2e55e8a
SirPhet <94946066+SirPhet@users.noreply.github.com>
2026-01-27 15:21:56 +0700

    update

---
+++ lab/app/src/main/java/com/example/a523lablearnandroid/MainActivity2.kt
@@ -0,0 +1,74 @@
+package com.example.a523lablearnandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a523lablearnandroid.ui.theme._523LabLearnAndroidTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+//            _523LabLearnAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android2",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+//            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Column() {
+        Row(modifier = Modifier.fillMaxWidth().background(Color.Blue)){
+            Text(
+                text = "Hello $name!",
+                modifier = modifier.height(50.dp).background(androidx.compose.ui.graphics.Color.Red)
+            )
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+        Column() {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    _523LabLearnAndroidTheme {
+        Greeting2("Android")
+    }
+}
