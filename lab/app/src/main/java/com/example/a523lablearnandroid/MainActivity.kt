956ff4aa39d2eff7f51398d01aa7579bfec8a67c
SirPhet <94946066+SirPhet@users.noreply.github.com>
2026-02-03 15:01:54 +0700

    upd

--- lab/app/src/main/java/com/example/a523lablearnandroid/MainActivity.kt
+++ lab/app/src/main/java/com/example/a523lablearnandroid/MainActivity.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a523lablearnandroid.ui.theme._523LabLearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,11 +61,50 @@ class MainActivity : ComponentActivity() {
                 }
 
                 //image
+                Image(
+                    painter = painterResource(id = R.drawable.tae),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(300.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top= 16.dp)
 
+
+                )
 
 
                 //status
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
 
+                )
+                {
+                    var str:Int by remember { mutableStateOf(10) }
+                    Column() {
+                        Button(onClick = {str = str+1 }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
+
+                        Text(text = "Str", fontSize = 32.sp)
+                        Text(text = str.toString(), fontSize = 32.sp)
+                        Text(text ="-",fontSize=32.sp,
+                            modifier = Modifier.clickable {
+                                str = str-1
+                            })
+
+                    }
+                    Column() {
+                        Text(text = "Agi", fontSize = 32.sp)
+                        Text(text = "8", fontSize = 32.sp)
+                    }
+                    Column() {
+                        Text(text = "Int", fontSize = 32.sp)
+                        Text(text = "8", fontSize = 32.sp)
+                    }
+                }
 
 
             }
