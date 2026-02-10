0d746a0a6b15159870cb49f37ea5a86add0fdbf1
phonlapat <94946066+SirPhet@users.noreply.github.com>
2026-02-10 15:03:55 +0700

    upd

--- lab/app/src/main/java/com/example/a523lablearnandroid/MenuActivity.kt
+++ lab/app/src/main/java/com/example/a523lablearnandroid/MenuActivity.kt
@@ -1,47 +1,44 @@
 package com.example.a523lablearnandroid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.a523lablearnandroid.ui.theme._523LabLearnAndroidTheme
+import androidx.compose.ui.unit.dp
 
 class MenuActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _523LabLearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+                Button(onClick = {
+                    startActivity(Intent(this@MenuActivity, MainActivity::class.java))
+                }) {
+                    Text(text = "RPGCardActivity")
+                }
+                Button(onClick = {
+                    startActivity(Intent(this@MenuActivity, MainActivity2::class.java))
+                }) {
+                    Text(text = "RPGCardActivity")
+                }
+                Button(onClick = {
+                    startActivity(Intent(this@MenuActivity, ListAcrivity::class.java))
+                }) {
+                    Text(text = "RPGCardActivity")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _523LabLearnAndroidTheme {
-        Greeting("Android")
     }
 }
